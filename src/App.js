2c6ec33637d5cb5dbcaddf9b875a2858d518deb7
Nabeel <nabeelahmed030@gmail.com>
2020-07-17 23:35:11 +0500

    Search Functionality added

--- src/App.js
+++ src/App.js
@@ -3,28 +3,32 @@ import './App.css';
 import Header from './components/Header';
 import Axios from 'axios';
 import CharacterGrid from './components/CharacterGrid';
+import Search from './components/Search';
 
 const App = () => {
 
   const [chars, setchars] = useState([])
   const [isLoading, setisLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
 
     const fetchData = async () => {
 
-      const result =  await Axios(`https://www.breakingbadapi.com/api/characters`)
-
+      const result =  await Axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
       setchars(result.data);
       setisLoading(false);
     }
 
     fetchData()
-  }, [])
+  }, [query])
 
   return (
     <div className="container">
      <Header />
+     <br />
+     <Search getQuery={(query) => setQuery(query)} />
+     <br />
      <CharacterGrid chars={chars} isLoading={isLoading} />
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,24 @@
 import React, {useState} from 'react'
 
-const Search = () => {
+const Search = ({ getQuery }) => {
     
-    const [text, settext] = useState('')
-    return (
+    const [text, setText] = useState('')
+
+    const onChange = (q) => {
+        setText(q)
+        getQuery(q)
+    } 
+
+    return (      
         <section className='search'>
-            
+            <form>
+                <input type='text'
+                        className='form-control'
+                        placeholder='Search Character by Name'
+                        value={text}
+                        onChange= {(e) => onChange(e.target.value)} 
+                        autoFocus />
+            </form>
         </section>
     )
 }
